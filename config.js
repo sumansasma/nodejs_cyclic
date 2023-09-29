e5132be35725f2d8912903c6d3cac82c28ab6f38
Suman <145822623+sumansasma@users.noreply.github.com>
2023-09-29 18:07:21 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+module.exports = {
+    nexmo: {
+        apiKey: '2b81a211',
+        apiSecret: 'wCzB7f1p5UrMCtXC',
+        phoneNumber: '919933012328', // This is the phone number associated with your Nexmo account
+    },
+};
