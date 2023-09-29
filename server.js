316eef14209ca1e8e36d27dcb1fb1d99bde377f8
Suman <145822623+sumansasma@users.noreply.github.com>
2023-09-29 20:20:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,33 @@ const cors = require('cors');
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+// Create a Nodemailer transporter
+const transporter = nodemailer.createTransport({
+    service: 'Gmail', // e.g., 'Gmail' or 'SendGrid'
+    auth: {
+        user: 'sijgeriaucssangha@gmail.com', // Replace with your email
+        pass: 'Sijgeria2003@' // Replace with your email password
+    }
+});
+
+function sendRegistrationConfirmation(email, participantName) {
+    const mailOptions = {
+        from: 'sijgeriaucssangha@gmail.com', // Replace with your email
+        to: email,
+        subject: 'Registration Confirmation',
+        text: `Hello ${participantName},\n\nThank you for registering for our event. Your registration is confirmed.\n\nRegards,\nYour Event Team`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -39,19 +66,6 @@ app.delete('/api/events/:eventId', (req, res) => {
     });
 });
 
-app.delete('/api/participants/:participantId', (req, res) => {
-    const participantId = req.params.participantId;
-
-    // Replace this with your actual database query to delete the participant by ID
-    db.run('DELETE FROM participants WHERE id = ?', [participantId], (err) => {
-        if (err) {
-            console.error(err.message);
-            return res.status(500).json({ message: 'Error deleting the participant.' });
-        }
-
-        res.status(200).json({ message: 'Participant deleted successfully.' });
-    });
-});
 
 
 // Create a SQLite database and initialize tables
@@ -126,33 +140,46 @@ app.get('/api/participants', (req, res) => {
         // Send a JSON response with the participants
         res.json(rows);
     });
+    const {  name, email } = req.body;
+
+    if ( !name  || !email) {
+        return res.status(400).json({ message: 'Event ID, name, phone number, and email are required.' });
+    }
+
+    db.run('INSERT INTO participants (eventId, name, phoneNumber, email) VALUES (?, ?, ?, ?)', [eventId, name, phoneNumber, email], function (err) {
+        if (err) {
+            console.error(err.message);
+            return res.status(500).json({ message: 'Participant registration failed.' });
+        }
+
+        sendRegistrationConfirmation(email, name); // Send confirmation email
+
+        res.status(201).json({ message: 'Participant registered successfully.', participantId: this.lastID });
+    });
 });
 
 
 // API endpoints for creating events and registering participants
 app.post('/api/events', (req, res) => {
-    const { name, date } = req.body;
+    const { name, phoneNumber, email } = req.body; // Extract data from the request body
 
-    if (!name || !date) {
-        return res.status(400).json({ message: 'Event name and date are required.' });
+    if (!name || !phoneNumber || !email) {
+        return res.status(400).json({ message: 'Event ID, name, phone number, and email are required.' });
     }
 
-    db.run('INSERT INTO events (name, date) VALUES (?, ?)', [name, date], function (err) {
+    db.run('INSERT INTO events (eventId, name, date) VALUES (?, ?, ?)', [eventId, name, date], function (err) {
         if (err) {
             console.error(err.message);
             return res.status(500).json({ message: 'Event creation failed.' });
         }
+
+        sendRegistrationConfirmation(email, name); // Send confirmation email
 
         res.status(201).json({ message: 'Event created successfully.', eventId: this.lastID });
     });
 });
 
 app.post('/api/participants', (req, res) => {
-    const { eventId, name, phoneNumber } = req.body;
-
-    if (!eventId || !name || !phoneNumber) {
-        return res.status(400).json({ message: 'Event ID, name, and phone number are required.' });
-    }
 
     db.run('INSERT INTO participants (eventId, name, phoneNumber) VALUES (?, ?, ?)', [eventId, name, phoneNumber], function (err) {
         if (err) {
@@ -163,6 +190,9 @@ app.post('/api/participants', (req, res) => {
         res.status(201).json({ message: 'Participant registered successfully.', participantId: this.lastID });
     });
 });
+
+module.exports = sendRegistrationConfirmation;
+
 
 // Start the server
 app.listen(port, () => {
